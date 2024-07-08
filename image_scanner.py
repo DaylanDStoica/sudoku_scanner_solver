4d2e9a6802a1dd0622856cdbb262d41304448041
DaylanDStoica <56146054+DaylanDStoica@users.noreply.github.com>
2024-07-08 15:57:01 -0700

    first two basic functions, not yet skeletons

--- image_scanner.py
+++ image_scanner.py
@@ -1,12 +1,17 @@
 # module for holding and using the image_scanner portion of the project
 # for scanning and interpreting the data into a more computer-readable format and file
 
+puzzle_image_filename = ""
+write_to_filename = "puzzle.txt"
 # 1. input an image file, currently png or jpeg
-
+def image_scanner():
+  print("open file for image scanning")
+  print( puzzle_image)
 # 2a. open the image file
 # 2b. scan the image file for patters
 # 2c. interpret the scan for placement of each number and separation. Track the filled and empty spaces of the image file
 
 # 3. write interpretation data to storage txt file
-
+def write_to_file():
+  print("open file " , write_to_filename )
 # 4. close image file
