3c8dea19e50d0250ec7d7031a5db144b1f176e93
DaylanDStoica <56146054+DaylanDStoica@users.noreply.github.com>
2024-07-08 15:46:05 -0700

    Create image_scanner.py, outline functions for writing

---
+++ image_scanner.py
@@ -0,0 +1,12 @@
+# module for holding and using the image_scanner portion of the project
+# for scanning and interpreting the data into a more computer-readable format and file
+
+# 1. input an image file, currently png or jpeg
+
+# 2a. open the image file
+# 2b. scan the image file for patters
+# 2c. interpret the scan for placement of each number and separation. Track the filled and empty spaces of the image file
+
+# 3. write interpretation data to storage txt file
+
+# 4. close image file
